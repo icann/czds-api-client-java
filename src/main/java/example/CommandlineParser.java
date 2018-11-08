48f734b877043c8a28263ca60cf4134921b440da
Dongmei Cao <dongmei.cao@icann.org>
2018-11-08 10:06:54 -0800

    More code refactoring: - Have a CommandlineParser to handle the command line options.

---
+++ src/main/java/example/CommandlineParser.java
@@ -0,0 +1,107 @@
+package example;
+
+import org.apache.commons.cli.*;
+import org.apache.commons.lang3.StringUtils;
+import org.icann.czds.sdk.model.ClientConfiguration;
+
+import java.io.IOException;
+
+public class CommandlineParser {
+
+    /**
+     * Build client
+     */
+    public static ClientConfiguration mergeCommandOptions(CommandLine commandLine) throws IOException {
+        // Lets get an instance of ClientConfiguration
+        ClientConfiguration configuration = null;
+        try {
+            configuration = ClientConfiguration.getInstance();
+        } catch (IOException e) {
+            System.out.println("ERROR: " + e.getMessage());
+            System.exit(1);
+        }
+
+        // Authetication base URL
+        if(commandLine.hasOption("authen-url")) {
+            configuration.setAuthenticationBaseUrl(commandLine.getOptionValue("authen-url"));
+        }
+
+        // CZDS base URL
+        if(commandLine.hasOption("czds-url")) {
+            configuration.setCzdsBaseUrl(commandLine.getOptionValue("czds-url"));
+        }
+
+        // Username
+        if(commandLine.hasOption("username")) {
+            configuration.setUserName(commandLine.getOptionValue("username"));
+        }
+
+        // Password
+        if(commandLine.hasOption("password")) {
+            configuration.setPassword(commandLine.getOptionValue("password"));
+        }
+
+        // Directory
+        String directory = null;
+        if(commandLine.hasOption("output")) {
+            configuration.setWorkingDirectory(commandLine.getOptionValue("directory"));
+        }
+
+        // Make sure all configurations are provided
+        String errorMsg = ClientConfiguration.validate();
+        if(!StringUtils.isBlank(errorMsg)) {
+            System.out.println("ERROR: missing the required configurations. " +
+                    "Please provide them in either application.properties file or pass in via command line options");
+            System.out.println(errorMsg);
+            System.exit(1);
+        }
+
+        return configuration;
+    }
+
+    /**
+     * Parse command line options
+     */
+    public static CommandLine parseCommandLineArguments(String[] args) {
+
+        Options options = new Options();
+
+        // tld option can take multiple arguments, comma separated
+        Option tldOption = new Option("t", "tld", true,
+                "Specify the TLD(s) you want to download zone file(s) for. " +
+                        "Comma separated multiple TLDs. By default, all APPROVED zone files will be downloaded.");
+        tldOption.setArgs(Option.UNLIMITED_VALUES);
+        tldOption.setValueSeparator(',');
+
+        options.addOption("u", "username", true, "Specify your username.")
+                .addOption("p", "password", true, "Specify your password")
+                .addOption("d", "directory", true, "Specify the working directory.")
+                .addOption("h", "help", false, "Print usage.")
+                .addOption("a", "authen-url", true, "Specify the authentication REST endpoint base URL.")
+                .addOption("c", "czds-url", true, "Specify the CZDS REST endpoint base URL.")
+                .addOption(tldOption);
+
+        // Create a parser
+        CommandLineParser parser = new DefaultParser();
+
+        // Parse the options passed as command line arguments
+        try {
+            CommandLine commandLine = parser.parse(options, args);
+
+            if (commandLine.hasOption("help")) {
+                printUsage(options);
+                System.exit(1);
+            }
+            return commandLine;
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            printUsage(options);
+            return null;
+        }
+    }
+
+    private static void printUsage(Options options) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("ZoneFileDownloader", options, true);
+    }
+}
--- src/main/java/example/ZoneFileDownloader.java
+++ src/main/java/example/ZoneFileDownloader.java
@@ -1,15 +1,15 @@
 package example;
 
-
 import org.apache.commons.cli.*;
-import org.apache.commons.lang3.StringUtils;
 import org.icann.czds.sdk.client.CzdsZoneDownloadClient;
 import org.icann.czds.sdk.model.AuthenticationException;
-import org.icann.czds.sdk.model.ClientConfiguration;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import static example.CommandlineParser.mergeCommandOptions;
+import static example.CommandlineParser.parseCommandLineArguments;
 
 /**
  * This application downloads zone files via CZDS REST endpoint
@@ -32,7 +32,7 @@ public class ZoneFileDownloader {
 
         // Build the REST API wrapper - CzdsZoneDownloadClient
         try {
-            client = buildCzdsClient(commandLine);
+            client = new CzdsZoneDownloadClient(mergeCommandOptions(commandLine));
         } catch (IOException e) {
             System.out.println("ERROR: " + e.getMessage());
             System.exit(1);
@@ -56,98 +56,6 @@ public class ZoneFileDownloader {
             System.out.println(e.getMessage());
             System.exit(1);
         }
-    }
-
-    /**
-     * Parse command line options
-     */
-    private CommandLine parseCommandLineArguments(String[] args) {
-
-        Options options = new Options();
-
-        // tld option can take multiple arguments, comma separated
-        Option tldOption = new Option("t", "tld", true,
-                "Specify the TLD(s) you want to download zone file(s) for. " +
-                        "Comma separated multiple TLDs. By default, all APPROVED zone files will be downloaded.");
-        tldOption.setArgs(Option.UNLIMITED_VALUES);
-        tldOption.setValueSeparator(',');
-
-        options.addOption("u", "username", true, "Specify your username.")
-                .addOption("p", "password", true, "Specify your password")
-                .addOption("d", "directory", true, "Specify the directory where the file(s) will be saved.")
-                .addOption("h", "help", false, "Print usage.")
-                .addOption("a", "authen-url", true, "Specify the authentication REST endpoint base URL.")
-                .addOption("c", "czds-url", true, "Specify the CZDS REST endpoint base URL.")
-                .addOption(tldOption);
-
-        // Create a parser
-        CommandLineParser parser = new DefaultParser();
-
-        // Parse the options passed as command line arguments
-        try {
-            CommandLine commandLine = parser.parse( options, args);
-
-            if(commandLine.hasOption("help")) {
-                printUsage(options);
-                System.exit(1);
-            }
-            return commandLine;
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-            printUsage(options);
-            return null;
-        }
-    }
-
-    /**
-     * Build client
-     */
-    private CzdsZoneDownloadClient buildCzdsClient(CommandLine commandLine) throws IOException {
-        // Lets get an instance of ClientConfiguration
-        ClientConfiguration configuration = null;
-        try {
-            configuration = ClientConfiguration.getInstance();
-        } catch (IOException e) {
-            System.out.println("ERROR: " + e.getMessage());
-            System.exit(1);
-        }
-
-        // Authetication base URL
-        if(commandLine.hasOption("authen-url")) {
-            configuration.setAuthenticationBaseUrl(commandLine.getOptionValue("authen-url"));
-        }
-
-        // CZDS base URL
-        if(commandLine.hasOption("czds-url")) {
-            configuration.setCzdsBaseUrl(commandLine.getOptionValue("czds-url"));
-        }
-
-        // Username
-        if(commandLine.hasOption("username")) {
-            configuration.setUserName(commandLine.getOptionValue("username"));
-        }
-
-        // Password
-        if(commandLine.hasOption("password")) {
-            configuration.setPassword(commandLine.getOptionValue("password"));
-        }
-
-        // Directory
-        String directory = null;
-        if(commandLine.hasOption("directory")) {
-            configuration.setWorkingDirectory(commandLine.getOptionValue("directory"));
-        }
-
-        // Make sure all configurations are provided
-        String errorMsg = ClientConfiguration.validate();
-        if(!StringUtils.isBlank(errorMsg)) {
-            System.out.println("ERROR: missing the required configurations. " +
-                    "Please provide them in either application.properties file or pass in via command line options");
-            System.out.println(errorMsg);
-            System.exit(1);
-        }
-
-        return new CzdsZoneDownloadClient(configuration);
     }
 
     /**
